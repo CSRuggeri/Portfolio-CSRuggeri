7da276663c61977b31b5fe16ffb61803e88f7505
CSRuggeri <santiruu@gmail.com>
2024-01-30 11:20:31 -0300

    xd

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -3,7 +3,7 @@ import "./profile.css"
 export const Profile = () => {
   return (
     <div className='profile'>
-        <img className='equisde' src="/assets/perfilProfesional.JPG" alt="santi" />
+        <img className='equisde' src="/public/assets/perfilProfesional.JPG" alt="santi" />
        <div className='info'>
         <h1>Carlos Santiago Ruggeri</h1>
         <h2>Full Stack Developer</h2>
