360fd420e11ab72b80c2f40349c1b1ccb488c157
CSRuggeri <santiruu@gmail.com>
2024-01-30 12:24:20 -0300

    cloudinary

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -3,7 +3,7 @@ import "./profile.css"
 export const Profile = () => {
   return (
     <div className='profile'>
-        <img className='equisde' src="assets/perfilProfesional.JPG" alt="santi" />
+        <img className='equisde' src="https://res.cloudinary.com/doxyqgk7w/image/upload/v1706627979/jybhhhbenrrlog4ezqrx.jpg" alt="santi" />
        <div className='info'>
         <h1>Carlos Santiago Ruggeri</h1>
         <h2>Full Stack Developer</h2>
