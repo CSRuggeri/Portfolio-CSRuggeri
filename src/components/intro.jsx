8ddbef632fed721d466b61eeb82e3e52d8c4bfac
CSRuggeri <santiruu@gmail.com>
2024-03-21 22:30:41 -0300

    txt

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -33,37 +33,36 @@ import ReactMarkdown from 'react-markdown';
     Welcome to my  Portfolio! 👋
     
     Hey, I'm Santiago Ruggeri,
-    a passionate and ambitious individual
-    on a journey through the dynamic world of programming. 
-    At 26 years old, 
-    I embarked on this exciting path just over a year ago,
-    and the learning adventure 
-    has been nothing short of exhilarating.
+   
+   
+    This is a page dedicated to navigate you through my tech career
+    im just begining a new chapter of my life
+    and in a short time i have acomplished a lot
     
-    In this short span,
-    I've successfully crafted three projects independently,
-    delving into various aspects of programming. 
-    Additionally, I've actively contributed
-    to two large group projects,
-    honing my collaborative skills
-    and gaining valuable experience in teamwork.
+    When i was nineteen i left my home 
+    to persue a carrer in psicology
+    but after four long years and the worlwide pandemic
+    i started to work in restaurants and bars
+    and gained lots of experience in client service and
+    managing gastronomic stablishments 
+
+    Then i realized a way of keep studying and working
+    of something that gives me passion
+    i was introduced to the wolrd of programing 
+    through Henry and then keept growing though 
+    several courses and programs like DigitalHouse 
+    and 
+
+    I intend to make this career a way of living
+    and i'm determinate to give my all to acomplish that.
     
-    Beyond the realm of coding,
-    I bring a unique blend of
-    experiences to the table. 
-    With a background in 
-    managing restaurants and bars,
-    I understand the importance of
-    efficiency, organization, and delivering
-    exceptional results in fast-paced environments. 🍔 🍹
+    Please share this portfolio so i can reech 
+    tech recruiters and companies that are intrested in working with me
     
-    This portfolio is a glimpse
-    into my evolving skill set, 
-    showcasing not only my technical
-    prowess but also my ability
-    to adapt and thrive in diverse settings. 
-    Join me as I continue to explore the limitless possibilities
-    of technology and leave an indelible mark on the digital landscape. 🌐
+      Tank You!
+
+    
+    . 🌐
     
     🚀🌟🚀Let's connect, collaborate, and create something extraordinary! 🚀🌟🚀 
     </span>)
